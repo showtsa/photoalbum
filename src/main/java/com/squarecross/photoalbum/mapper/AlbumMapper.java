7ab9e3445c36d3a7307a6a6d2da0dc3426147233
gyeongbae <showtsa@naver.com>
2024-02-25 03:13:23 +0900

    썸네일 경로 추가

--- src/main/java/com/squarecross/photoalbum/mapper/AlbumMapper.java
+++ src/main/java/com/squarecross/photoalbum/mapper/AlbumMapper.java
@@ -3,6 +3,9 @@ package com.squarecross.photoalbum.mapper;
 import com.squarecross.photoalbum.domain.Album;
 import com.squarecross.photoalbum.dto.AlbumDto;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class AlbumMapper {
     public static AlbumDto convertToDto(Album album) {
         AlbumDto albumDto = new AlbumDto();
@@ -10,5 +13,17 @@ public class AlbumMapper {
         albumDto.setAlbumName(album.getAlbumName());
         albumDto.setCreatedAt(album.getCreatedAt());
         return albumDto;
+    }
+
+    public static Album convertToModel(AlbumDto albumDto) {
+        Album album = new Album();
+        album.setAlbumId(albumDto.getAlbumId());
+        album.setAlbumName(albumDto.getAlbumName());
+        album.setCreatedAt(albumDto.getCreatedAt());
+        return album;
+    }
+
+    public static List<AlbumDto> convertToDtoList(List<Album> albums) {
+        return albums.stream().map(AlbumMapper::convertToDto).collect(Collectors.toList());
     }
 }
