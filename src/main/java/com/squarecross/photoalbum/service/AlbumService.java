7d1db8692652ff0cda73de470089739ce1fdbc72
gyeongbae <showtsa@naver.com>
2024-02-25 00:29:04 +0900

    service, repository 파일 생성

---
+++ src/main/java/com/squarecross/photoalbum/service/AlbumService.java
@@ -0,0 +1,42 @@
+package com.squarecross.photoalbum.service;
+
+import com.squarecross.photoalbum.domain.Album;
+import com.squarecross.photoalbum.dto.AlbumDto;
+import com.squarecross.photoalbum.mapper.AlbumMapper;
+import com.squarecross.photoalbum.repository.AlbumRepository;
+import com.squarecross.photoalbum.repository.PhotoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.persistence.EntityNotFoundException;
+import java.util.Optional;
+
+@Service
+public class AlbumService {
+    @Autowired
+    private AlbumRepository albumRepository;
+
+    @Autowired
+    private PhotoRepository photoRepository;
+
+    public AlbumDto getAlbum(Long albumId){
+        Optional<Album> res = albumRepository.findById(albumId);
+        if (res.isPresent()){
+            AlbumDto albumDto = AlbumMapper.convertToDto(res.get());
+            albumDto.setCount(photoRepository.countByAlbum_AlbumId(albumId));
+            return albumDto;
+        } else {
+            throw new EntityNotFoundException(String.format("앨범 아이디 %d로 조회되지 않았습니다", albumId));
+        }
+    }
+
+    public AlbumDto getAlbumByAlbumName(String albumName) {
+        Optional<Album> res = albumRepository.findByAlbumName(albumName);
+        if (res.isPresent()){
+            AlbumDto albumDto = AlbumMapper.convertToDto(res.get());
+            return albumDto;
+        } else {
+            throw new EntityNotFoundException(String.format("앨범명 %s로 조회되지 않았습니다", albumName));
+        }
+    }
+}
